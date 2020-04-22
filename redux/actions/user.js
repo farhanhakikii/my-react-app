ec2e9e31297f67d577899e2fc9c3ee687b7c08b0
farhanhakikii <61401271+farhanhakikii@users.noreply.github.com>
2020-04-22 14:17:26 +0700

    Add files via upload

--- redux/actions/user.js
+++ redux/actions/user.js
@@ -1,6 +1,77 @@
+import Axios from "axios"
+import { API_URL } from "../../constants/API"
+
 export const userInputHandler = (text) => {
     return {
         type: "ON_CHANGE_USER_INPUT",
         payload: text
     }
-} 
+}
+
+export const regHandler = (regdata) => {
+    return (dispatch) => {
+        const {username,password,role,fullName} = regdata
+        Axios.get(`${API_URL}/pengguna?un=${username}`)
+        .then(res => {
+            if(username == '' || password == '' || role == '' || fullName == ''){
+                alert("Lengkapi form registrasi")
+            }else if(res.data.length > 0){
+                alert("Username telah terdaftar")
+            }else{
+                Axios.post(`${API_URL}/pengguna`,{
+                    un: username,
+                    pw: password,
+                    rl: role,
+                    fn: fullName
+                   })
+                alert("Registrasi Berhasil")
+                dispatch({
+                    type: "ON_LOGIN_SUCCESS",
+                    payload: res.data[0]
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+}
+
+export const loginHandler = (datauser) => {
+    return (dispatch) => {
+        const {username,password} = datauser
+        Axios.get(`${API_URL}/pengguna`, {
+            params: {
+                un: username,
+                pw: password
+            }
+        })
+        .then(res => {
+            if(res.data.length > 0){
+                alert("sukses")
+                dispatch({
+                    type: "ON_LOGIN_SUCCESS",
+                    payload: res.data[0]
+                })
+            }else {
+                alert("gagal")
+                dispatch({
+                    type: "ON_LOGIN_FAILED",
+                    payload: "username/password salah"
+                })
+            }
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        // dispatch({
+        //     type: "TESTING",
+        //     payload: "Hello World"
+        // })        
+        // dispatch({
+        //     type: "TESTING2",
+        //     payload: "Hello Worldas"
+        // })
+    }
+}
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,12 +1,36 @@
-const init_state = {
-    id: "0",
-    username: "User"
-}
-
-export default (state=init_state, action) => {
-    if(action.type === "ON_CHANGE_USER_INPUT"){
-        return { ...state, username: action.payload }
-    }else{
-        return { ...state }
-    }
-    }
+    const init_state = {
+        id: 0,
+        username: "admin",
+        fullName: "",
+        role: "",
+        testing: "",
+        testing2: "",
+        errmsg: ""
+    };
+    
+    export default (state = init_state, action) => {
+        if (action.type === "ON_CHANGE_USER_INPUT") {
+            return { ...state, username: action.payload };
+        }else if (action.type === "ON_LOGIN_SUCCESS") {
+            const {un, fn, rl} = action.payload
+            return { ...state, un, fn, rl };
+        }
+        return { ...state };
+    };
+    
+    // const init_state = {
+    //     id: "0",
+    //     username: "User",
+    //     fullName: "",
+    //     role: "",
+    //     testing: "",
+    //     testing2: ""
+    // }
+    
+    // export default (state=init_state, action) => {
+    //     if(action.type === "ON_CHANGE_USER_INPUT"){
+    //         return { ...state, username: action.payload }
+    //     }else if(action.type === "TESTING"){
+    //         return { ...state }
+    //     }
+    //     }
