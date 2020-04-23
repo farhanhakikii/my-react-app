57add3d6a10d8e9fb7c6081f9eb9a020f7626c4a
farhanhakikii <61401271+farhanhakikii@users.noreply.github.com>
2020-04-23 11:59:01 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,47 @@ import './App.css';
 import './bootstrap.css'
 import AuthScreen from './views/screen/AuthScreen';
 import LifeCycleScreen from './LifeCycleScreen';
+import { connect } from 'react-redux'
 import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
 import HomeScreen from './views/screen/HomeScreen';
+import cookie from 'universal-cookie'
 import CounterScreen from './views/screen/CounterScreen'
 import PageNotFound from './views/screen/PageNotFound';
 import Navbar from './views/components/Navbar';
 import InputScreen from './views/screen/InputScreen';
 import ProfileScreen from './views/WeeklyTask/ProfileScreen';
-import RegScreen from './views/WeeklyTask/RegScreen';
+import RegisterScreen from './views/WeeklyTask/RegisterScreen';
 import LoginScreen from './views/WeeklyTask/LoginScreen';
+import LogScreen from './views/WeeklyTask/LogScreen';
+import TodoReduxScreen from './views/screen/TodoReduxScreen';
+import { keepLogin } from './redux/actions'
 
-function App() {
-  return (
+const cookieObject = new cookie()
+class App extends React.Component{
+
+  componentDidMount(){
+    let cookieResult = cookieObject.get("authData")
+    if(cookieResult){
+      this.props.keepLogin(cookieResult)  
+    }
+  }
+ render() {
+   return (
     <BrowserRouter>
       <Navbar/>
+      {/* <TodoReduxScreen/> */}
       <Switch>
-        <Route exact path="/" component={RegScreen}/>
+        <Route exact path="/todo" component={TodoReduxScreen}/>
+        <Route exact path="/input" component={InputScreen}/>
+        <Route exact path="/" component={RegisterScreen}/>
         <Route exact path="/login" component={LoginScreen}/>
         <Route exact path="/profile/:username" component={ProfileScreen}/>
         <Route path="*" component={PageNotFound}/>{/*path="*" cocok untuk semua,jadi apapun hasilnya bakal nampilin error not found, tapi kalo yg diatasnya udh ketemu dulu, maka ga dieksekusi*/}
       </Switch>
     </BrowserRouter>
-  );
+
+   )
+ }
 }
 
 //<Route exact path="/" component={HomeScreen}/>{/*EXACT BIAR PATH YG SAMA PERSIS YG DITAMPILIN, SOALNYA KALO GAPAKE EXACT UDH KETEMU YG COCOK LANGSUNG DITAMPILIN*/}
@@ -34,5 +53,14 @@ function App() {
 <Route exact path="/input" component={InputScreen}/> */}
 //<Route exact path="/profile/:username" component={ProfileScreen /*:username adalah nama parameter yg akan di ambil oleh profilescreen setelah welcome, ...*/}/>
 
+const mapStatetoProps = (state) => {
+  return{
+    user: state.user
+  }
+}
 
-export default withRouter(App);
+const mapDispatchtoProps = {
+  keepLogin,
+}
+
+export default connect(mapStatetoProps,mapDispatchtoProps)(withRouter(App));
